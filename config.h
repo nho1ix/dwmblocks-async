d92e29daa0243893eb490fe820af4a1422f83122
Victor Vo <tupilbewb@gmail.com>
2022-10-22 00:53:27 -0700

    Add mail and stock pricing to statusbar

--- config.h
+++ config.h
@@ -3,6 +3,8 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
+	BLOCK("spxs",       18000,   23),
+	BLOCK("sb-mail",    18000,   28),
 	BLOCK("sb-disk",    14400,   17),
 	BLOCK("sb-memory",      1,   18),
 	BLOCK("sb-cpu",         1,   19),
