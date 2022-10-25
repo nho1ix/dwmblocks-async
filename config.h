bd504b51fa89f07c91caabbf6b7e0cdebc153741
Victor Vo <tupilbewb@gmail.com>
2022-10-24 21:35:30 -0700

    Add zram & stock (WIP) modules

--- config.h
+++ config.h
@@ -3,9 +3,10 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("spxs",       18000,   23),
+	BLOCK("sb-spxs",    18000,   23),
 	BLOCK("sb-mail",    18000,   28),
 	BLOCK("sb-disk",    14400,   17),
+	BLOCK("sb-swap",        1,   24),
 	BLOCK("sb-memory",      1,   18),
 	BLOCK("sb-cpu",         1,   19),
 	BLOCK("sb-weather", 18000,   20),
