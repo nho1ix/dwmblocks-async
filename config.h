23ecd72dd7ec7502281e99813a8e0597f36dabe3
Victor Vo <tupilbewb@gmail.com>
2022-11-03 20:23:00 -0700

    Fix pos/neg stock_dc color, add variable for stock script

--- config.h
+++ config.h
@@ -3,15 +3,15 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-spxs",    18000,   23),
+	BLOCK("sb-stock",    3600,   23),
 	// BLOCK("sb-mail",    18000,   28),
-	BLOCK("sb-disk",    14400,   17),
+	BLOCK("sb-disk",        0,   17),
 	BLOCK("sb-swap",        1,   24),
 	BLOCK("sb-memory",      1,   18),
 	BLOCK("sb-cpu",         1,   19),
-	BLOCK("sb-weather", 18000,   20),
+	BLOCK("sb-weather",     0,   20),
 	BLOCK("sb-volume",      0,   10),
 	// BLOCK("sb-battery",     120,  3),
 	BLOCK("sb-clock",       1,   26),
-	BLOCK("sb-internet",    7,   27),
+	BLOCK("sb-internet",    6,   27),
 };
