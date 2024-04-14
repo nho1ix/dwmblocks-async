e5ec9bb47647738fbd4ad587e980677381bb229a
Victor Vo <fromvictorvo@gmail.com>
2024-04-14 12:49:11 -0700

    Update

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 #define DELIMITER "  "
 
 // Maximum number of Unicode characters that a block can output.
-#define MAX_BLOCK_OUTPUT_LENGTH 100 
+#define MAX_BLOCK_OUTPUT_LENGTH 64
 
 // Control whether blocks are clickable.
 #define CLICKABLE_BLOCKS 1
@@ -16,19 +16,13 @@
 // Control whether a trailing delimiter should be appended to the status.
 #define TRAILING_DELIMITER 0
 
-#define BLOCKS(X)         \
-	/* X("sb-stock",    3600,   23) \ */ \
-	/* X("sb-mail",    18000,   28) \ */ \
-	/* X("sb-music_name",     0,   22) */ \
-	/* X("sb-music_time",     1,   23) */ \
-	X("sb-weather",     18000,   20) \
-	X("sb-rain",     0,   21) \
-	X("sb-disk",        0,   17) \
-	X("sb-swap",        1,   24) \
-	X("sb-memory",      1,   18) \
-	X("sb-cpu",         1,   19) \
-	X("sb-volume",      0,   10) \
-	/* X("sb-battery",     120,  3) */ \
-	X("sb-clock",       1,   26) \
-	X("sb-internet",    6,   27)
+#define BLOCKS(X)           \
+	X("sb-weather", 3600, 20) \
+	X("sb-disk", 0, 17)       \
+	/* X("sb-swap", 1, 24) */      \
+	X("sb-memory", 1, 18)     \
+	X("sb-cpu", 1, 19)        \
+	X("sb-volume", 0, 10)     \
+	X("sb-clock", 1, 26)      \
+	X("sb-internet", 6, 27)
 #endif  // CONFIG_H
